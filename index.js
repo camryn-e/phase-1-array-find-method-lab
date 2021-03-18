57cf61ed445c85d40a279a9d295adfd6a6ca255b
camryn williams <williams.camryn.e@gmail.com>
2021-03-18 16:44:14 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function isWin(elem, value, record){
+    console.log(elem['result']);
+    return (elem['result'] === 'W');
+}
+
+function superbowlWin(record){
+    const winYear = record.find(elem => elem['result'] === 'W');
+    return winYear ? winYear['year'] : undefined;
+}
